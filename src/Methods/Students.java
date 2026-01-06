d4eb7fa3a6b4ab51dd21425fbfed993c8e2393b3
anupam <anupamk537@gmail.com>
2026-01-06 18:53:34 +0530

    Methods

---
+++ src/Methods/Students.java
@@ -0,0 +1,31 @@
+package Methods;
+
+public class Students{
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        Students s=new Students();
+        s.hello();
+        Students.greet();
+        System.out.println(s.sum(1,4));
+        System.out.println( s.Isodd(7));
+       s.checkOddEven(12);
+    }
+    void hello(){
+        System.out.println("instance method");
+    }
+    static void greet(){
+        System.out.println("Static method");
+    }
+    int sum(int a, int b){
+        return a+b;
+    }
+    boolean Isodd(int num){
+            return num%2 !=0;
+    }
+    void checkOddEven(int num){
+        if(num%2==0)
+            System.out.println("even");
+        else
+        System.out.println("odd");
+    }
+}
